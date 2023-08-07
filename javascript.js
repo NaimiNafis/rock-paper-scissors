3c8235004f145a80ad5367fc5f42bf2aee0e477c
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-08-07 18:11:04 +0900

    Add error message in playerSelection

--- javascript.js
+++ javascript.js
@@ -28,9 +28,21 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = prompt("Choose between rock, paper, and scissors");
+function getPlayerChoice() {
+  const input = prompt(
+    "Choose between rock, paper, and scissors",
+    ""
+  ).toLowerCase();
+  if (input == "rock" || input == "paper" || input == "scissors") {
+    return input;
+  }
+  console.error("Invalid Input!");
+}
+
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+console.log(computerSelection);
 
 /*
 PROGRAM getComputerChoice
@@ -42,65 +54,37 @@ END
 
 PROGRAM playerSelection
     Prompt player to choose either rock, paper, scissors
-    RETURN choice;
+    IF player type wrong choices THEN
+        return "Please choose either rock, paper, scissors"
+    ELSE
+        RETURN choice;
 END
 
 PROGRAM playRound
-    Get playerSelection and computerSelection;
-    Compare both selection;
-    let say if player choose rock;
-    IF computer choose rock THEN
-        Print Tie;
-    ELSEIF computer choose scissors THEN
-        Print Lose;
-    ELSE computer choose paper THEN
-        Print Win;
+    Get playerSelection and computerSelection input;
+    IF player === computer THEN
+        Return "tie";
+    ELSE
+        IF player choose rock THEN
+            IF computer choose paper THEN
+                Print Lose;
+
+
+        IF player choose scissors THEN
+            IF computer choose rock THEN
+                Print Lose;
+
+
+        IF player choose paper THEN
+            IF computer choose scissors THEN
+                Print Lose;
+        ELSE 
+            Print Win;
 END
 
 PROGRAM game
     5 TIMES DO
 
-    IF player === computer THEN
-    Return "tie";
-
-    IF player choose rock THEN
-        ELSEIF computer choose paper THEN
-            Print Lose;
-        ELSE computer choose scissors THEN
-            Print Win;
-
-    IF player choose scissors THEN
-        ELSEIF computer choose rock THEN
-            Print Lose;
-        ELSE computer choose paper THEN
-            Print Win;
-
-    IF player choose paper THEN
-        ELSEIF computer choose scissors THEN
-            Print Lose;
-        ELSE computer choose rock THEN
-            Print Win;
 END
 
-
-*/
-
-/*
-(get user input)
-(compare with computer rand)
-1.check if same
-
-if player == rock
-    if(comp == paper)
-        return lose
-
-if player == paper
-    if(comp == scissors)
-        return lose
-
-if player == scissors
-    if(comp == rock)
-        return lose
-
-return win     
 */
