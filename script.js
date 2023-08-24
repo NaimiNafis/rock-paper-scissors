2f0c8d3c76fd80f1d103525b2ea7ed6c712f5380
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-08-24 18:23:11 +0900

    Add rock paper scissors buttons and eventlisteners Add scores and round variables Add DOM for text in scoreboard Modify playRound function Add updateScore function Delete scoreboard child div in html Change first word button ids to capital Style footer and button

--- script.js
+++ script.js
@@ -1,180 +1,158 @@
+//Computer and Player Choice
+
+const rock = document.getElementById('Rock');
+const paper = document.getElementById('Paper');
+const scissors = document.getElementById('Scissors');
+
+let playerScore = 0;
+let compScore = 0;
+let round = 1;
+
+const scoreboard = document.getElementById('scoreboard');
+
+let decider = document.createElement('div');
+decider.textContent = 'Choose Your Weapon';
+scoreboard.appendChild(decider);
+
+let description = document.createElement('div');
+description.textContent = 'First to score 5 points wins the game';
+scoreboard.appendChild(description);
+
+let playerScoreText = document.createElement('div');
+playerScoreText.textContent = `Player: ${compScore}`;
+scoreboard.appendChild(playerScoreText);
+
+let compScoreText = document.createElement('div');
+compScoreText.textContent = `Computer: ${compScore}`;
+scoreboard.appendChild(compScoreText);
+
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
-function getPlayerChoice() {
-  if (document.querySelector('button').textContent === 'rock'){
-    return 'rock';
-  } else if (document.querySelector('button').textContent === 'paper'){
-    return 'paper';
-  } else {
-    return 'scissors';
-  }
-}
+rock.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "Rock";
+  playRound(playerSelection, computerSelection);
+  updateScore();
+});
+
+paper.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "Paper";
+  playRound(playerSelection, computerSelection);
+  updateScore();
+});
+
+scissors.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const playerSelection = "Scissors";
+  playRound(playerSelection, computerSelection);
+  updateScore();
+});
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      return "You Lose! Paper beats Rock";
-    } else {
-      return `You Win! Rock beats ${computerSelection}`;
-    }
+  if (playerSelection === computerSelection) {
+    decider.textContent = "It's a Tie!";
+    description.textContent = `${playerSelection} ties with ${computerSelection}`
   }
-  if (playerSelection === "paper") {
-    if (computerSelection === "scissors") {
-      return "You Lose! Scissors beats Paper";
-    } else {
-      return `You Win! Paper beats ${computerSelection}`;
-    }
-  }
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      return "You Lose! Rock beats Scissors";
-    } else {
-      return `You Win! Scissors beats ${computerSelection}`;
-    }
-  }
-}
-
-function updateRound(resultText){
-  const scoreText = `Player : ${playerScore} | Computer : ${computerScore}`;
-  const roundText = `Round : ${round}`;
-  result.textContent = `${resultText} \n ${scoreText} \n ${roundText}`;
-
-  if (playerScore === 5){
-    result.textContent += " " + "Congratulations! You killin' it!!";
-  } 
-  else if (computerScore === 5){
-    result.textContent += " " + "You Lose against Computer lol! Better Luck Next Time!";
-  }
-}
-
-let playerScore = 0;
-let computerScore = 0;
-let round = 0;
-
-const scoreboard = document.getElementById('scoreboard')
-const result = document.createElement('div');
-scoreboard.appendChild(result);
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const resultText = playRound(playerSelection, computerSelection);
-    if(resultText.includes('You Win')){
+  else if (playerSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      decider.textContent = 'You Won!';
+      description.textContent = `Rock beats ${computerSelection}`;
       playerScore++;
-    }else if(resultText.includes('You Lose')){
-      computerScore++;
+    } else {
+      decider.textContent = 'You Lose!';
+      description.textContent = `Rock is beaten by ${computerSelection}`;
+      compScore++;
     }
-    round++;
-    updateRound(resultText);
-  })
-});
+  }
+  else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      decider.textContent = 'You Won!';
+      description.textContent = `Paper beats ${computerSelection}`;
+      playerScore++;
+    } else {
+      decider.textContent = 'You Lose!';
+      description.textContent = `Paper is beaten by ${computerSelection}`;
+      compScore++;
+    }
+  }
+  else if (playerSelection === "Scissors") {
+    if (computerSelection === "Paper") {
+      decider.textContent = 'You Won!';
+      description.textContent = `Scissors beats ${computerSelection}`;
+      playerScore++;
+    } else {
+      decider.textContent = 'You Lose!';
+      description.textContent = `Scissors is beaten by ${computerSelection}`;
+      compScore++;
+    }
+  }
+  round++;
+}
+
+function updateScore() {
+  if (decider.textContent === 'You Won!') {
+    playerScore++;
+  }
+  else if (decider.textContent === 'You Lose!') {
+    compScore++;
+  }
+}
 
 
 /*
 
-we want player and comp score, round;
-
-so set 3 var,
-set playerScore and compScore and round
-
-const playerScore = 0;
-const compScore = 0;
-const round = 0;
-
-IF player win THEN
-  result.textContent = 'You win';
-  playerScore++;
-ElSE comp win THEN 
-  result.textContent = 'You Lose';
-  compScore++;
-
-  create new function, return that to result;
-
-from resultText, we got player and comp selection,
-so for 
-  round
-
-  who win
-
-
-
-
-
-
-
 PROGRAM rps-ui
+
+//Get playerSelection
   Create 3 button
-  Assign rock, paper, scissors on each button
+    Assign rock, paper, scissors on each button
+    IF rock button clicked THEN
+    RETURN rock
+    IF paper button clicked THEN
+    RETURN paper
+    IF scissors button clicked THEN
+    RETURN scissors
 
-  Create random computer generator choices
-
-  Create user choices function
-    how to connect button and user input?
-    IF user click rps button THEN
-    RETURN choice
-
-  Create playRound function
-    Compare comp and user input
-    RETURN output
-
-
-
-
-PROGRAM getComputerChoice
+//Get computerSelection
+  Generate random choices
     Generate 3 Choices (Rock, Paper, Scissors);
     Generate random index number;
     Make the random index number decide which in the array wil be choosen;
-    RETURN
-END
+    RETURN either rock, paper, scissors
+  
+//Compare both input playRound()
+  IF playerSelection === computerSelection THEN
+    tie
 
-PROGRAM getPlayerChoice
-    Prompt player to choose either rock, paper, scissors
-    IF player type wrong choices THEN
-        return "Please choose either rock, paper, scissors"
-    ELSE
-        RETURN choice;
-END
+  IF rock button is clicked (which means 'rock' returned) THEN
+    IF computerSelection === 'scissors' THEN
+      Win
+    ELSE IF computerSelection === 'paper' THEN
+      Lose
+  
+  IF paper button is clicked (which means 'paper' returned) THEN
+    IF computerSelection === 'rock' THEN
+      Win
+    ELSE IF computerSelection === 'scissors' THEN
+      Lose
 
-PROGRAM playRound
-    Get playerSelection and computerSelection input;
-    IF player === computer THEN
-        Return "tie";
-    ELSE
-        IF player choose rock THEN
-            IF computer choose paper THEN
-                Print Lose;
+  IF scissors button is clicked (which means 'scissors' returned) THEN
+    IF computerSelection === 'paper' THEN
+      Win
+    ELSE IF computerSelection === 'rock' THEN
+      Lose
 
-
-        IF player choose scissors THEN
-            IF computer choose rock THEN
-                Print Lose;
-
-
-        IF player choose paper THEN
-            IF computer choose scissors THEN
-                Print Lose;
-        ELSE 
-            Print Win;
-END
-
-PROGRAM game
-    5 TIMES DO all from start
+//In DOM scoreboard
+Need 5 div
+1. You Win! / You Lose! / It's a Tie!
+2. Reason (exp : Rock is beaten by Paper / Rock ties with rock/ Rock beats scissors)
+3. img that show playerSelection plus userScore
+4. img that show computerSelection plus compScore
 
 END
-
-
-  const input = prompt(
-    "Choose between rock, paper, and scissors",
-    ""
-  ).toLowerCase();
-  if (input == "rock" || input == "paper" || input == "scissors") {
-    return input;
-  }
-  console.error("Invalid Input!");
 */
